5df8a23c41541b1868fa2d253a717130767ae44f
s2andynguyen <thanhtungnguyen9911@gmail.com>
2023-12-26 22:37:34 +0700

    setup

---
+++ app/auth/page.tsx
@@ -0,0 +1,9 @@
+export default function Auth() {
+  return (
+    <div className="relative h-full w-full bg-site-auth bg-no-repeat bg-cover">
+      <div className="opacity-50 h-full">
+          auth
+      </div>
+    </div>
+  )
+}
