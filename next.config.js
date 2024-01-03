1852f8d7918e7c04ecd78556e489c4651e0e9454
s2andynguyen <thanhtungnguyen9911@gmail.com>
2024-01-03 19:45:03 +0700

    fix next.config

--- next.config.js
+++ next.config.js
@@ -1,39 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'mango.blender.org',
-            port: '',
-            pathname: '/**',
-          },
-          {
-            protocol: 'http',
-            hostname: 'uhdtv.io',
-            port: '',
-            pathname: '/**',
-          },
-          {
-            protocol: 'https',
-            hostname: 'download.blender.org',
-            port: '',
-            pathname: '/**',
-          },
-          {
-            protocol: 'https',
-            hostname: 'upload.wikimedia.org',
-            port: '',
-            pathname: '/**',
-          },
-          {
-            protocol: 'https',
-            hostname: 'lh3.googleusercontent.com',
-            port: '',
-            pathname: '/**',
-          }
-        ],
-      },
+  reactStrictMode: true,
+  images: {
+    domains: [
+      "mango.blender.org",
+      "uhdtv.io",
+      "download.blender.org",
+      "upload.wikimedia.org",
+      "lh3.googleusercontent.com"
+    ],
+  },
 }
 
 module.exports = nextConfig
